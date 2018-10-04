c1aed7c9d01913e7e9c634696e2dfdf12f7acbad
Anwesh43 <anweshthecool0@gmail.com>
2018-10-04 08:14:51 +0530

    adding drawSSWNode canvas extension method

--- stepsqwaveview/src/main/java/com/anwesh/uiprojects/stepsqwaveview/StepSqWaveView.kt
+++ stepsqwaveview/src/main/java/com/anwesh/uiprojects/stepsqwaveview/StepSqWaveView.kt
@@ -13,3 +13,26 @@ import android.graphics.Color
 import android.content.Context
 
 val nodes : Int = 5
+
+fun Canvas.drawSSWNode(i : Int, scale : Float, paint : Paint) {
+    paint.color = Color.parseColor("#4CAF50")
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap = Paint.Cap.ROUND
+    val gap : Float = w / (nodes)
+    save()
+    translate(gap * i, h/2)
+    for (j in 0..1) {
+        val x : Float = gap/2 * j
+        val sf : Float = 1f - 2 * j
+        val sc : Float = Math.min(0.5f, Math.max(scale - 0.5f, 0f)) * 2
+        val path : Path = Path()
+        path.moveTo(x, 0f)
+        path.lineTo(x, gap/4 * sf * sc)
+        path.lineTo(x + gap/2, gap/4 * sf * sc)
+        path.lineTo(x+gap/2, 0f)
+        drawPath(path, paint)
+    }
+    restore()
+}
