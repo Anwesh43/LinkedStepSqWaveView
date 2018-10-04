3e79b9653b8ebeae116ba7f04fd88a0ea3aac173
Anwesh43 <anweshthecool0@gmail.com>
2018-10-04 08:06:18 +0530

    adding libraries and defining nodes

--- stepsqwaveview/src/main/java/com/anwesh/uiprojects/stepsqwaveview/StepSqWaveView.kt
+++ stepsqwaveview/src/main/java/com/anwesh/uiprojects/stepsqwaveview/StepSqWaveView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.stepsqwaveview
 /**
  * Created by anweshmishra on 04/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
