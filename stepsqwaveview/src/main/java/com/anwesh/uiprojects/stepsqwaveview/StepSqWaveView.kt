4dd64f4093ee871e754378eb3cb867b703a3ad30
Anwesh43 <anweshthecool0@gmail.com>
2018-10-04 08:36:01 +0530

    adding StepSqWave class

--- stepsqwaveview/src/main/java/com/anwesh/uiprojects/stepsqwaveview/StepSqWaveView.kt
+++ stepsqwaveview/src/main/java/com/anwesh/uiprojects/stepsqwaveview/StepSqWaveView.kt
@@ -143,4 +143,28 @@ class StepSqWaveView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class StepSqWave(var i : Int) {
+
+        private var root : SSWNode = SSWNode(0)
+        private var curr : SSWNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
